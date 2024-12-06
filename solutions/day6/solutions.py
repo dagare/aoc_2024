eb095311a8ca2b7b01604a739753ba447f5f4b7c
Dag-Are Trydal <dagaretrydal@gmail.com>
2024-12-06 10:17:05 +0100

    wip

--- solutions/day6/solutions.py
+++ solutions/day6/solutions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy
 
 SIMPLE_INPUT_1 = [
 '.#..',
@@ -53,6 +54,14 @@ class Guard:
         self.pos = (i, j)
         self.dir = dir
 
+    def __eq__(self, other):
+        if not isinstance(other, Guard):
+            return NotImplemented
+        return self.pos == other.pos and self.dir == other.dir
+
+    def __repr__(self):
+        return f"Guard(pos={self.pos}, dir={self.dir})"
+    
 class Map:
     def __init__(self, lines):
         self.map = []
@@ -61,15 +70,11 @@ class Map:
             if '.' in line:
                 self.map.append(list(line.strip()))
 
-        self.array = np.array(self.map, dtype='<U1')
+        # self.array = np.array(self.map, dtype='<U1')
 
         self.map_size = (len(self.map), len(self.map[0]))
 
-        a = 0
-        b = a +1
-        guard = self.find_guard()
-
-        c = 0
+        self.previous_guard_states = []
 
 
     def find_guard(self):
@@ -82,6 +87,7 @@ class Map:
         return None
         
     def move_guard(self, guard): 
+        self.previous_guard_states.append(guard)
 
         next_position = None
 
@@ -110,8 +116,6 @@ class Map:
         # Set state to visited
         if self.map[next_position[0]][next_position[1]] == '.': 
             self.map[next_position[0]][next_position[1]] = 'x'
-        # elif self.map[next_position[0]][next_position[1]] in '<>^v': 
-        #     self.map[next_position[0]][next_position[1]] = 'x'
 
         guard.pos = next_position
 
@@ -129,6 +133,13 @@ class Map:
             return True
         
         return False
+    
+    def is_in_infinite_loop(self, guard):
+
+        if guard in self.previous_guard_states:
+            return True
+        
+        return False
 
     def summarize_visited_cells(self):
         sum = 0
@@ -136,8 +147,12 @@ class Map:
             for j in range(self.map_size[1]):
                 if self.map[i][j] == 'x':
                     sum += 1
-                elif self.map[i][j] in '<>^v':
-                    sum += 1
+                # elif self.map[i][j] in '<>^v':
+                #     sum += 1
+
+        # just add one for the start position
+        sum +=1 
+
         return sum
                     
 
@@ -151,25 +166,52 @@ def solve_part1(map):
 
     return map.summarize_visited_cells()
 
-def solve_part2(map):
-    
-    sum = 0
+def solve_part2(lines):
+    map = Map(lines)
 
-    return sum
+        
+    infinite_loop_cnt = 0
+    infinite_loop_positions = []
+
+    for i in range(map.map_size[0]):
+        for j in range(map.map_size[1]):
+            if map.map[i][j] == '.':
+                temp_map = Map(lines)
+                temp_map.map[i][j] = 'x'
+                guard = temp_map.find_guard()
+            
+                (guard, is_done) = (guard, False)
+                while not is_done:
+                    (guard, is_done) = temp_map.move_guard(guard)
+                    
+                    if temp_map.is_in_infinite_loop(guard):
+                        infinite_loop_positions.append((i, j))
+                        infinite_loop_cnt += 1
+                        break
+
+    return infinite_loop_cnt
 
 
 def main():
     simple_map_1 = Map(SIMPLE_INPUT_1)
-    p1 = solve_part1(simple_map_1)
-    print(f'Part 1 (simple_1): {p1} Correct: {p1==4}')
+    p11 = solve_part1(simple_map_1)
+    print(f'Part 1 (simple_1): {p11} Correct: {p11==4}')
     simple_map_2 = Map(SIMPLE_INPUT_2)
-    p1 = solve_part1(simple_map_2)
-    print(f'Part 1 (simple_2): {p1} Correct: {p1==5}')
+    p12 = solve_part1(simple_map_2)
+    print(f'Part 1 (simple_2): {p12} Correct: {p12==5}')
 
     example_map = Map(EXAMPLE_INPUT)
     p1 = solve_part1(example_map)
     print(f'Part 1 (example): {p1} Correct: {p1==41}')
-    p2 = solve_part2(example_map)
+
+
+    p21 = solve_part2(SIMPLE_INPUT_1)
+    print(f'Part 2 (simple_1): {p21} Correct: {p21==0}')
+    p22 = solve_part2(SIMPLE_INPUT_1)
+    print(f'Part 2 (simple_2): {p22} Correct: {p22==0}')
+
+
+    p2 = solve_part2(EXAMPLE_INPUT)
     print(f'Part 2 (example): {p2} Correct: {p2==6}')
 
     input_file = "solutions/day6/input.txt"
@@ -177,7 +219,7 @@ def main():
     map = Map(lines)
 
     print("\nPart 1:", solve_part1(map))
-    print("\nPart 2:", solve_part2(map))
+    print("\nPart 2:", solve_part2(lines))
 
 if __name__ == "__main__":
     main()
